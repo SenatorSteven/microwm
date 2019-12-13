ce7a395c27bc9dbcba35c416ef60b36e4dee0adc
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2019-12-13 21:09:37 +0200

    Update microwm.c

--- microwm.c
+++ microwm.c
@@ -2,16 +2,10 @@
 #include <X11/Xlib.h>
 #include "headers/defines.h"
 
-#define NoPositions /*----*/ 0
-#define ConfigPosition /*-*/ (1 << 0)
-#define HelpPosition /*---*/ (1 << 1)
-#define ExitPosition /*---*/ (1 << 2)
-
 #define ModeContinue /*---*/ ((unsigned int)0)
 #define ModeRestart /*----*/ ((unsigned int)1)
 #define ModeExit /*-------*/ ((unsigned int)2)
 
-static unsigned int getParameters(const int *const argumentCount, const char *const *const argumentVector, const char **configPath);
 static void eventLoop(Display *const display, unsigned int *const mode);
 
 int main(const int argumentCount, const char *const *const argumentVector){
@@ -31,83 +25,6 @@ int main(const int argumentCount, const char *const *const argumentVector){
 		}
 	}
 	return 0;
-}
-static unsigned int isArgument(const char *const argument, const char *const argumentArray){
-	unsigned int value = 0;
-	unsigned int length = 0;
-	while(argument[length] > '\0'){
-		length++;
-	}
-	unsigned int currentCharacter = 0;
-	while(currentCharacter < length){
-		if(argument[currentCharacter] >= 'A' && argument[currentCharacter] <= 'Z'){
-			if(!(argumentArray[currentCharacter] == argument[currentCharacter] || argumentArray[currentCharacter] == argument[currentCharacter] + 32)){
-				break;
-			}
-		}else if(argument[currentCharacter] >= 'a' && argument[currentCharacter] <= 'z'){
-			if(!(argumentArray[currentCharacter] == argument[currentCharacter] || argumentArray[currentCharacter] == argument[currentCharacter] - 32)){
-				break;
-			}
-		}else{
-			if(!(argumentArray[currentCharacter] == argument[currentCharacter])){
-				break;
-			}
-		}
-		currentCharacter++;
-	}
-	if(currentCharacter == length){
-		value = 1;
-	}
-	return value;
-}
-static unsigned int getParameters(const int *const argumentCount, const char *const *const argumentVector, const char **configPath){
-	const int dereferencedArgumentCount = *argumentCount;
-	unsigned int value = 0;
-	if(dereferencedArgumentCount > 1){
-		unsigned int hasReadVariable = NoPositions;
-		for(int currentArgument = 1; currentArgument < dereferencedArgumentCount; currentArgument++){
-			if(!(hasReadVariable & ConfigPosition)){
-				if(isArgument("-c", argumentVector[currentArgument]) || isArgument("--config", argumentVector[currentArgument])){
-					if(argumentVector[currentArgument + 1]){
-						currentArgument++;
-							if(isArgument("-h", argumentVector[currentArgument]) || isArgument("--help", argumentVector[currentArgument])){
-							hasReadVariable |= ExitPosition;
-							fprintf(stdout, "%s: usage: %s --config \"/path/to/file/\"\n", ProgramName, ProgramName);
-							fprintf(stdout, "   # if the specified file doesn't exist, it will be created and it will contain the hardcoded default configuration\n");
-							fprintf(stdout, "   # the $HOME variable can be used instead of \"/path/to/home/\", case sensitive\n");
-							break;
-						}else{
-							*configPath = (char *)argumentVector[currentArgument];
-							hasReadVariable |= ConfigPosition;
-							continue;
-						}
-					}else{
-						fprintf(stderr, "%s: no config value specified\n", ProgramName);
-						hasReadVariable |= ExitPosition;
-						break;
-					}
-				}
-			}
-			if(!(hasReadVariable & HelpPosition)){
-				if(isArgument("-h", argumentVector[currentArgument]) || isArgument("--help", argumentVector[currentArgument])){
-					fprintf(stdout, "%s: usage: %s [parameters] or %s [parameter] [--help]\n", ProgramName, ProgramName, ProgramName);
-					fprintf(stdout, "   [-h], [--help]     display this message\n");
-					fprintf(stdout, "   [-c], [--config]   specify path to config, necessary\n");
-					hasReadVariable |= HelpPosition;
-					break;
-				}
-			}
-			fprintf(stderr, "%s: \"%s\" is not recognized as program parameter, check help? [-h]\n", ProgramName, argumentVector[currentArgument]);
-			hasReadVariable |= ExitPosition;
-			break;
-		}
-		if(hasReadVariable & ConfigPosition && !(hasReadVariable & HelpPosition) && !(hasReadVariable & ExitPosition)){
-			value = 1;
-		}
-	}else{
-		fprintf(stderr, "%s: no config parameter specified\n", ProgramName);
-	}
-	return value;
 }
 static void eventLoop(Display *const display, unsigned int *const mode){
 	XGrabKey(display, 9, Mod4Mask, XDefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
