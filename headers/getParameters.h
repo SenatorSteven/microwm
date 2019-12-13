399be267436b20942d752c5ad6b7549c2cecca89
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2019-12-13 21:10:07 +0200

    Create getParameters.h

---
+++ headers/getParameters.h
@@ -0,0 +1,6 @@
+#ifndef GETPARAMETERS_H
+#define GETPARAMETERS_H
+
+unsigned int getParameters(const int *const argumentCount, const char *const *const argumentVector, const char **configPath);
+
+#endif
