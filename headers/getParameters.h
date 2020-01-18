688837bfcab4e38769ed1a5e8d5cf59621bea90e
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2020-01-18 16:11:49 +0200

    Delete getParameters.h

--- headers/getParameters.h
+++
@@ -1,6 +0,0 @@
-#ifndef GETPARAMETERS_H
-#define GETPARAMETERS_H
-
-unsigned int getParameters(const unsigned int *const argumentCount, const char *const *const argumentVector, const char **configPath);
-
-#endif
